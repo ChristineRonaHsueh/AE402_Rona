15faa334bae5f9fea963a3b335e2828e3eb1875f
ChristineRonaHsueh <41505585+ChristineRonaHsueh@users.noreply.github.com>
2021-04-10 15:36:38 +0800

    Add files via upload

---
+++ star_tower.py
@@ -0,0 +1,54 @@
+##print
+print('Hello')
+
+#變數
+a = 'David'
+print(a)
+
+#型別
+x = 1
+y = 2.6
+z = 'apple'
+print(type(y))
+
+#print相加
+print(x,y,z)
+
+#while
+a = 0
+while a<3:
+    print('abc')
+    a = a+1
+
+#課堂示範星星塔
+i=1
+while i<6:
+    print("*"*i)
+    i= i+1
+
+#實作時間 –星星塔(10層)
+k = 1
+while k<11:
+    print("*"*k)
+    k+=1
+
+
+#挑戰時間 –倒星星塔
+j = 5
+while j>0:
+    print("*"*j)
+    j = j-1
+
+#回家作業 –倒星星塔(10層)
+n = 10
+while n>0:
+    print("*"*n)
+    n=n-1
+
+
+
+
+
+
+
+    
